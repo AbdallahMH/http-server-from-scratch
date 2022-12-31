4a8a43ab2ba6f1f5ed28a403ffec0c3fe7e1cc75
abdallahmh <abdallahhezam1@gmail.com>
2022-12-31 16:53:00 +0300

    add rake Lobster

--- server.rb
+++ server.rb
@@ -3,33 +3,56 @@ require "active_support/all"
 require_relative "request"
 require_relative "response"
 
+require "rack"
+require "rack/lobster"
+
+APP = Rack::Lobster.new
+
 port = ENV.fetch("PORT", 2000).to_i
 server = TCPServer.new 2000
 
 puts "listening on port #{port}..."
 
+def render(file:)
+  body = File.binread(file)
+  Response.new(
+    code: 200,
+    body: body,
+    headers: {
+      "Content-Length" => body.bytesize,
+      "Content-Type" => "text/html",
+    })
+end
 
-def render(path)
-    full_path = File.join(__dir__, "views", path)
-    if File.exist?(full_path)
-        Response.new(code: 200, body: File.binread(full_path))
-    else
-        Response.new(code: 404)
-    end
+def template_exists?(path)
+  File.exists?(path)
 end
 
 def route(request)
-    if request.path == "/"
-        render "index.html"
-    else
-        render request.path
-    end
+  path = (request.path == "/") ? "index.html" : request.path
+  full_path = File.join(__dir__, "views", path)
+
+  if template_exists?(full_path)
+    render file: full_path
+  else
+    status, headers, body = APP.call({
+                                       "REQUEST_METHOD" => request.method,
+                                       "PATH_INFO" => request.path,
+                                       "QUERY_STRING" => request.query,
+                                     })
+
+    Response.new(code: status, body: body.join, headers: headers)
+  end
+rescue => e
+  puts e.full_message
+  Response.new(code: 500)
 end
+
 loop do
-    Thread.start(server.accept) do |client|
-        request = Request.new client.readpartial(2048)
-        response = route(request)
-        response.send(client)
-        client.close
-    end
+  Thread.start(server.accept) do |client|
+    request = Request.new client.readpartial(2048)
+    response = route(request)
+    response.send(client)
+    client.close
+  end
 end
