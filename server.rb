e00a107ca2f8d06b91815ee2298d73aed77bd994
abdallahmh <abdallahhezam1@gmail.com>
2022-12-31 17:07:32 +0300

    Add Rails application

--- server.rb
+++ server.rb
@@ -6,7 +6,26 @@ require_relative "response"
 require "rack"
 require "rack/lobster"
 
-APP = Rack::Lobster.new
+require 'rails'
+require 'action_controller/railtie'
+
+class SingleFile < Rails::Application
+  config.session_store :cookie_store, :key => '_session'
+  config.secret_key_base = '7893aeb3427daf48502ba09ff695da9ceb3c27daf48b0bba09df'
+  Rails.logger = Logger.new($stdout)
+end
+
+class PagesController < ActionController::Base
+  def index
+    render inline: "<h1>Hello World!</h1> <p>I'm just a single file Rails application</p>"
+  end
+end
+
+SingleFile.routes.draw do
+  root to: "pages#index"
+end
+
+APP = SingleFile
 
 port = ENV.fetch("PORT", 2000).to_i
 server = TCPServer.new 2000
@@ -28,7 +47,7 @@ def template_exists?(path)
   File.exists?(path)
 end
 
-def route(request)
+def route(request, client)
   path = (request.path == "/") ? "index.html" : request.path
   full_path = File.join(__dir__, "views", path)
 
@@ -39,6 +58,10 @@ def route(request)
                                        "REQUEST_METHOD" => request.method,
                                        "PATH_INFO" => request.path,
                                        "QUERY_STRING" => request.query,
+                                       "SERVER_NAME" => "localhost",
+                                       "SERVER_PORT" => 2000,
+                                       "HTTP_HOST" => "localhost",
+                                       "rack.input" => client,
                                      })
 
     Response.new(code: status, body: body.join, headers: headers)
@@ -51,7 +74,7 @@ end
 loop do
   Thread.start(server.accept) do |client|
     request = Request.new client.readpartial(2048)
-    response = route(request)
+    response = route(request, client)
     response.send(client)
     client.close
   end
